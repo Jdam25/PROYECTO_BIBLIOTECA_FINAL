74d9185524b51f9906e0ca7b9a9aaf4f8692d8e5
AngieEs12 <90420690+AngieEs12@users.noreply.github.com>
2021-11-30 00:08:09 -0600

    Angie Espinal

--- VISTAS/FACTURA.Designer.cs
+++ VISTAS/FACTURA.Designer.cs
@@ -39,9 +39,9 @@ namespace PROYECTO_BIBLIOTECA.VISTAS
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(0, -27);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(575, 212);
+            this.dataGridView1.Size = new System.Drawing.Size(769, 212);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
@@ -49,7 +49,7 @@ namespace PROYECTO_BIBLIOTECA.VISTAS
             this.button1.BackColor = System.Drawing.Color.DarkGray;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
-            this.button1.Location = new System.Drawing.Point(200, 250);
+            this.button1.Location = new System.Drawing.Point(301, 248);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(158, 23);
             this.button1.TabIndex = 1;
@@ -60,7 +60,8 @@ namespace PROYECTO_BIBLIOTECA.VISTAS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(573, 392);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(767, 365);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FACTURA";
