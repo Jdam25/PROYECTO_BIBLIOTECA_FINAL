6a42a52a5daf83a0b538c484e51bcbf819d090a7
Jdam25 <90662905+Jdam25@users.noreply.github.com>
2021-11-30 10:36:59 -0600

    Avila_Fabricio

--- VISTAS/FACTURA.Designer.cs
+++ VISTAS/FACTURA.Designer.cs
@@ -31,6 +31,8 @@ namespace PROYECTO_BIBLIOTECA.VISTAS
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,9 +41,9 @@ namespace PROYECTO_BIBLIOTECA.VISTAS
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, -27);
+            this.dataGridView1.Location = new System.Drawing.Point(-4, 153);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(769, 212);
+            this.dataGridView1.Size = new System.Drawing.Size(774, 212);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
@@ -49,12 +51,36 @@ namespace PROYECTO_BIBLIOTECA.VISTAS
             this.button1.BackColor = System.Drawing.Color.DarkGray;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
-            this.button1.Location = new System.Drawing.Point(301, 248);
+            this.button1.Location = new System.Drawing.Point(608, 59);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.Size = new System.Drawing.Size(101, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "SALIR";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(457, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "\"La lectura hace al hombre completo; \r\nla conversación lo hace ágil, el escribir " +
+    "lo hace preciso\".";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(13, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Francis Bacon, escritor y filósofo.";
             // 
             // FACTURA
             // 
@@ -62,12 +88,15 @@ namespace PROYECTO_BIBLIOTECA.VISTAS
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(767, 365);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FACTURA";
             this.Text = "FACTURA";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,5 +104,7 @@ namespace PROYECTO_BIBLIOTECA.VISTAS
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- VISTAS/FACTURA.cs
+++ VISTAS/FACTURA.cs
@@ -16,5 +16,10 @@ namespace PROYECTO_BIBLIOTECA.VISTAS
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
